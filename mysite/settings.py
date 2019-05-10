48a7663b3ce62705a9511ab9d576678838c33fac
Savio Joanes <joanes.savio@gmail.com>
2019-05-10 00:08:36 -0400

    remove src

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,22 +52,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
@@ -119,5 +103,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-ACCOUNT_LOGOUT_REDIRECT_URL = "/myapp"
--- src/exercises/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExercisesConfig(AppConfig):
-    name = 'exercises'
