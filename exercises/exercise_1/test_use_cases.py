e1ba1ff5436796e29541e413c8164af53c8ce85e
Savio Joanes <joanes.savio@gmail.com>
2019-05-09 21:20:40 -0400

    add models

---
+++ exercises/exercise_1/test_use_cases.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.mark.django_db()
+def test_get_all_professors():
+    pass
--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Professor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Textbook(models.Model):
+    title = models.CharField(max_length=30)
+    author = models.ForeignKey(Professor, on_delete=models.CASCADE)
+
