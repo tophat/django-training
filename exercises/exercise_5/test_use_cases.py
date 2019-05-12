2fea482438589dce9a02865b23a4cd8272975464
Savio Joanes <joanes.savio@gmail.com>
2019-05-12 18:59:03 -0400

    add comment

--- exercises/exercise_5/test_use_cases.py
+++ exercises/exercise_5/test_use_cases.py
@@ -5,5 +5,8 @@ from exercises.exercise_5.use_cases import update_professor_ages
 
 @pytest.mark.django_db()
 def test_(django_assert_num_queries):
+    """
+    hint: https://docs.djangoproject.com/en/2.2/ref/models/expressions/#f-expressions
+    """
     with django_assert_num_queries(1):
         update_professor_ages()
