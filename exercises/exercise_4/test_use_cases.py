4fe2010eecf568179ff4d8ad667e9c44745c96cc
Savio Joanes <joanes.savio@gmail.com>
2019-05-27 23:54:59 -0400

    refactor: e4

--- exercises/exercise_4/test_use_cases.py
+++ exercises/exercise_4/test_use_cases.py
@@ -1,13 +1,18 @@
 import pytest
 
 from exercises.exercise_4.use_cases import add_new_professors
+from exercises.models import Professor
 
 
 @pytest.mark.django_db()
 def test_add_new_professors(django_assert_num_queries):
+    expected_names = [
+        ("Steve", "Pezos"),
+        ("Charlies", "Xavier"),
+        ("Samuel", "Oak")
+    ]
     with django_assert_num_queries(1):
-        add_new_professors([
-            ("Steve", "Pezos"),
-            ("Charlies", "Xavier"),
-            ("Samuel", "Oak")
-        ])
+        add_new_professors(expected_names)
+    for names, prof in zip(expected_names, Professor.objects.all()):
+        assert names[0] == prof.first_name
+        assert names[1] == prof.last_name
--- exercises/exercise_4/use_cases.py
+++ exercises/exercise_4/use_cases.py
@@ -1,7 +1,15 @@
+from typing import Tuple, List
+
 from exercises.models import Professor
 
+FullName = Tuple[str, str]
 
-def add_new_professors(names):
+
+def add_new_professors(names: List[FullName]):
+    """
+    Creates multiple professors
+    :param names: Tuple of first and last names
+    """
     for first_name, last_name in names:
         Professor.objects.create(
             first_name=first_name,
