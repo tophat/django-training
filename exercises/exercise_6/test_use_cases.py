99b17065b42eaee491689b7f979331bf8c3a53e6
Savio Joanes <joanes.savio@gmail.com>
2019-05-28 00:05:12 -0400

    refactor: e6

--- exercises/exercise_6/test_use_cases.py
+++ exercises/exercise_6/test_use_cases.py
@@ -1,8 +1,18 @@
 import pytest
 
 from exercises.exercise_6.use_cases import get_professor
+from exercises.models import Professor
+
+
+@pytest.fixture
+def profs():
+    Professor.objects.create(first_name="joe", last_name="Shmoe")
+    Professor.objects.create(first_name="a", last_name="ElRair")
+    Professor.objects.create(first_name="will", last_name="Arner")
 
 
 @pytest.mark.django_db()
-def test_get_professor():
-    assert "SCAN TABLE" not in get_professor("joe").explain()
+def test_get_professor(profs):
+    profs = get_professor("joe")
+    assert profs[0].first_name == "joe"
+    assert "SCAN TABLE" not in profs.explain()
--- exercises/exercise_6/use_cases.py
+++ exercises/exercise_6/use_cases.py
@@ -1,7 +1,9 @@
-from django.db.models.query import QuerySet
-
 from exercises.models import Professor
 
 
-def get_professor(first_name: str) -> QuerySet:
+def get_professor(first_name: str) -> Professor:
+    """
+    :param first_name: Specified first name
+    :return: Professor with a matching first name
+    """
     return Professor.objects.filter(first_name=first_name)
