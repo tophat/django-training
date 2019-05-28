c0921bfb6a96c134899e43834e3d6427bd095e46
Savio Joanes <joanes.savio@gmail.com>
2019-05-28 00:13:29 -0400

    refactor: e7

--- exercises/exercise_7/test_use_cases.py
+++ exercises/exercise_7/test_use_cases.py
@@ -1,9 +1,24 @@
 import pytest
 
 from exercises.exercise_7.use_cases import get_professors
+from exercises.models import Professor
+
+
+@pytest.fixture
+def profs():
+    Professor.objects.create(first_name="freddie", last_name="mercury")
+    Professor.objects.create(first_name="brian", last_name="may")
+    Professor.objects.create(first_name="john", last_name="deacon")
+    Professor.objects.create(first_name="roger", last_name="taylor")
+    Professor.objects.create(first_name="charles", last_name="taylor")
 
 
 @pytest.mark.django_db()
-def test_get_professors(django_assert_num_queries):
+def test_get_professors(django_assert_num_queries, profs):
+    prof_ids = [1, 2, 3, 4]
     with django_assert_num_queries(1):
-        get_professors([1, 2, 23, 41])
+        group = get_professors(prof_ids)
+        assert len(group) == len(prof_ids)
+        for prof, prof_id in zip(group, prof_ids):
+            assert isinstance(prof, Professor)
+            assert prof.id == prof_id
